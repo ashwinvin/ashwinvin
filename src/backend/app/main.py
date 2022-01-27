61896e0a5b64c46553829b703e259313c925d11d
Ashwin Vinod <ashwinvinodsa@gmail.com>
2022-01-27 08:31:09 +0530

    add project path and split templates

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -6,31 +6,30 @@ from starlite.template import JinjaTemplateEngine
 
 
 @get(path="/")
-async def index(repos: List[str]) -> Template:
+async def index() -> Template:
+    return Template(name="index.html", context={})
+
+@get(path="/projects")
+async def projects(repos: List[str]) -> Template:
     data = {"repos": {}}
     async with httpx.AsyncClient() as client:
         for repo in repos:
             response = await client.get(f"https://api.github.com/repos/{repo}")
             print(response.status_code)
             repo_data = response.json()
-            data["repos"].update(
-                {
-                    repo_data["name"]: {
+            data["repos"][repo_data["name"]] = {
                         "stars": repo_data["stargazers_count"],
                         "description": repo_data["description"],
-                        "link": repo_data["url"],
+                        "link": repo_data["html_url"],
                     }
-                }
-            )
-    return Template(name="index.html", context=data)
-
+    return Template(name="projects.html", context=data)
 
 def get_config() -> List[str]:
     return ["ashwinvin/visionlib"]
 
 
 app = Starlite(
-    route_handlers=[index],
+    route_handlers=[index, projects],
     static_files_config=[StaticFilesConfig(directories=["../frontend"], path="/static")],
     template_config=TemplateConfig(directory="../frontend", engine=JinjaTemplateEngine),
     dependencies={"repos": Provide(get_config)},
